0407d8048f40bae38673e79a06535ba594cbc7fe
a.v <averdugo@csumb.edu>
2021-03-26 18:50:42 -0700

    added abstract

---
+++ src/LibraryTest.java
@@ -0,0 +1,93 @@
+/**
+ 1.Title:LibraryTest
+ 2.Abstract: Tests Library Class
+ 3.Author: Alexander Verdugo
+ 4.Date: March 25th 2021 // Bad idea to start the day before I am so ready for spring break
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void convertInt() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void listBooks() {
+    }
+
+    @Test
+    void checkOutBook() {
+    }
+
+    @Test
+    void getBookByISBN() {
+    }
+
+    @Test
+    void testReturnBook() {
+    }
+
+    @Test
+    void listShelves() {
+    }
+
+    @Test
+    void getShelf() {
+    }
+
+    @Test
+    void testGetShelf() {
+    }
+
+    @Test
+    void listReaders() {
+    }
+
+    @Test
+    void testListReaders() {
+    }
+
+    @Test
+    void addShelf() {
+    }
+
+    @Test
+    void getReaderByCard() {
+    }
+
+    @Test
+    void addReader() {
+    }
+
+    @Test
+    void removeReader() {
+    }
+
+    @Test
+    void testAddShelf() {
+    }
+
+    @Test
+    void convertDate() {
+    }
+
+    @Test
+    void getLibraryCardNumber() {
+    }
+}
